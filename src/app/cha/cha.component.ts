6b70710d57c9622bc280b8765c524ddcc9161e7d
CITIGO\datnt1 <dat.nt4@citigo.com.vn>
2021-11-18 14:43:36 +0700

    complete CHA-CON

--- src/app/cha/cha.component.ts
+++ src/app/cha/cha.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ConComponent } from '../con/con.component';
 
 @Component({
   selector: 'app-cha',
   templateUrl: './cha.component.html',
-  styleUrls: ['./cha.component.scss']
+  styleUrls: ['./cha.component.scss'],
 })
 export class ChaComponent implements OnInit {
+  public chatMessage = '';
+  @ViewChild('thangTien') thangTien: ConComponent | undefined;
+  @ViewChild('thangDat') thangDat: ConComponent | undefined;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  public conGoi(name: string) {
+    this.chatMessage = 'Tao nghe ' + name + ' ơi';
+    console.log(' Gọi gì đấy ' + name + ' ơi');
   }
 
+  public xinTien() {
+    this.thangTien?.xinTien(60);
+    this.thangDat?.xinTien(100);
+  }
 }
